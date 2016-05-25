4e04695df1c4ada06735ed3a0219241be03c5362
Roman Gelman <rgel75@gmail.com>
2016-05-25 10:18:05 +0300

    Update Vi-Module.psm1

--- Vi-Module/Vi-Module.psm1
+++ Vi-Module/Vi-Module.psm1
@@ -18,7 +18,7 @@ Function Get-RDM {
 	C:\PS> $res |Export-Csv -NoTypeInformation 'C:\reports\ProdCluster_RDMs.csv'
 	Save the results in variable and than export them to a file.
 .INPUTS
-	[VMware.VimAutomation.ViCore.Impl.V1.Inventory.VirtualMachineImpl[]] Get-VM collection.
+	[VMware.VimAutomation.ViCore.Types.V1.Inventory.VirtualMachine[]] Get-VM collection.
 .OUTPUTS
 	[System.Management.Automation.PSCustomObject] PSObject collection.
 .NOTES
@@ -38,7 +38,7 @@ Param (
 	[Parameter(Mandatory=$false,Position=1,ValueFromPipeline=$true,HelpMessage="VM's collection, returned by Get-VM cmdlet")]
 		[ValidateNotNullorEmpty()]
 		[Alias("VM")]
-	[VMware.VimAutomation.ViCore.Impl.V1.Inventory.VirtualMachineImpl[]]$VMs = (Get-VM)
+	[VMware.VimAutomation.ViCore.Types.V1.Inventory.VirtualMachine[]]$VMs = (Get-VM)
 
 )
 
@@ -115,7 +115,7 @@ Function Convert-VmdkThin2EZThick {
 	C:\PS> Get-VM VM1,VM2 |Convert-VmdkThin2EZThick -Confirm:$false |sort VM,Datastore,VMDK |ft -au
 .INPUTS
 	Get-VM collection.
-	[VMware.VimAutomation.ViCore.Impl.V1.Inventory.VirtualMachineImpl[]]
+	[VMware.VimAutomation.ViCore.Types.V1.Inventory.VirtualMachine[]]
 .OUTPUTS
 	PSObject collection.
 .NOTES
@@ -131,7 +131,7 @@ Param (
 	[Parameter(Mandatory=$true,Position=1,ValueFromPipeline=$true,HelpMessage="VM's collection, returned by Get-VM cmdlet")]
 		[ValidateNotNullorEmpty()]
 		[Alias("VM")]
-	[VMware.VimAutomation.ViCore.Impl.V1.Inventory.VirtualMachineImpl[]]$VMs
+	[VMware.VimAutomation.ViCore.Types.V1.Inventory.VirtualMachine[]]$VMs
 
 )
 
@@ -383,7 +383,7 @@ Filter Get-VMHostFirmwareVersion {
 .EXAMPLE
 	PS C:\> 'esxprd1.domain.com','esxdev2' |Get-VMHostFirmwareVersion
 .INPUTS
-	[VMware.VimAutomation.ViCore.Impl.V1.Inventory.VMHostImpl[]] Objects, returned by Get-VMHost cmdlet.
+	[VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost[]] Objects, returned by Get-VMHost cmdlet.
 	[VMware.Vim.HostSystem[]] Objects, returned by Get-View cmdlet.
 	[System.String[]] ESXi hostname or FQDN.
 .OUTPUTS
@@ -535,7 +535,7 @@ Filter Get-VMHostBirthday {
 	Format output using ToString() method.
 	http://blogs.technet.com/b/heyscriptingguy/archive/2015/01/22/formatting-date-strings-with-powershell.aspx
 .INPUTS
-	[VMware.VimAutomation.ViCore.Impl.V1.Inventory.VMHostImpl[]] Objects, returned by Get-VMHost cmdlet.
+	[VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost[]] Objects, returned by Get-VMHost cmdlet.
 	[System.String[]] ESXi hostname or FQDN.
 .OUTPUTS
 	[System.DateTime[]] ESXi installation date/time.
@@ -629,7 +629,7 @@ Process {
 
 }
 
-}
+} #EndFunction Enable-VMHostSSH
 New-Alias -Name Enable-ViMVMHostSSH -Value Enable-VMHostSSH -Force:$true
 
 Function Disable-VMHostSSH {
@@ -710,7 +710,7 @@ Process {
 
 }
 
-}
+} #EndFunction Disable-VMHostSSH
 New-Alias -Name Disable-ViMVMHostSSH -Value Disable-VMHostSSH -Force:$true
 
 Function Set-VMHostNtpServer {
@@ -736,7 +736,7 @@ Function Set-VMHostNtpServer {
 	PS C:\> Get-VMHost -Location Datacenter1 |sort Name |Set-VMHostNtpServer -NewNtp 'ntp1','ntp2' |epcsv -notype -Path '.\Ntp_report.csv'
 	Export the results to Excel.
 .INPUTS
-	[VMware.VimAutomation.ViCore.Impl.V1.Inventory.VMHostImpl[]] VMHost collection returned by Get-VMHost cmdlet.
+	[VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost[]] VMHost collection returned by Get-VMHost cmdlet.
 .OUTPUTS
 	[System.Management.Automation.PSCustomObject] PSObject collection.
 .NOTES
@@ -752,7 +752,7 @@ Param (
 
 	[Parameter(Mandatory=$false,Position=1,ValueFromPipeline=$true)]
 		[ValidateNotNullorEmpty()]
-	[VMware.VimAutomation.ViCore.Impl.V1.Inventory.VMHostImpl[]]$VMHost = (Get-VMHost)
+	[VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost[]]$VMHost = (Get-VMHost)
 	,
 	[Parameter(Mandatory,Position=2)]
 	[System.String[]]$NewNtp
@@ -815,5 +815,408 @@ Process {
 
 } #EndFunction Set-VMHostNtpServer
 New-Alias -Name Set-ViMVMHostNtpServer -Value Set-VMHostNtpServer -Force:$true
+
+Function Get-Version {
+
+<#
+.SYNOPSIS
+	Get VMware Virtual Infrastructure objects' version info.
+.DESCRIPTION
+	This cmdlet gets VMware Virtual Infrastructure objects' version info.
+.PARAMETER VIObject
+	Vitual Infrastructure objects (VM, VMHosts, DVSwitches, Datastores).
+.PARAMETER VCenter
+	Get versions for all connected VCenter servers/ESXi hosts and PowerCLi version on the localhost.
+.PARAMETER LicenseKey
+	Get versions of license keys.
+.EXAMPLE
+	PS C:\> Get-VMHost |Get-Version |? {$_.Version -ge 5.5 -and $_.Version.Revision -lt 2456374}
+	Get all ESXi v5.5 hosts that have Revision less than 2456374.
+.EXAMPLE
+	PS C:\> Get-View -ViewType HostSystem |Get-Version |select ProductName,Version |sort Version |group Version |sort Count |select Count,@{N='Version';E={$_.Name}},@{N='VMHost';E={($_.Group |select -expand ProductName) -join ','}} |epcsv -notype 'C:\reports\ESXi_Version.csv'
+	Group all ESXi hosts by Version and export the list to CSV.
+.EXAMPLE
+	PS C:\> Get-VM |Get-Version |? {$_.FullVersion -match 'v10' -and $_.Version -gt 9.1}
+	Get all VM with Virtual Hardware v10 and VMTools version above v9.1.0.
+.EXAMPLE
+	PS C:\> Get-Version -VCenter |Format-Table -AutoSize
+	Get all connected VCenter servers/ESXi hosts versions and PowerCLi version.
+.EXAMPLE
+	PS C:\> Get-DistributedSwitch |Get-Version |sort Version |? {$_.Version -lt 5.5}
+	Get all DVSwitches that have version below 5.5.
+.EXAMPLE
+	PS C:\> Get-Datastore |Get-Version |? {$_.Version.Major -eq 3}
+	Get all VMFS3 datastores.
+.EXAMPLE
+	PS C:\> Get-Version -LicenseKey
+	Get license keys version info.
+.INPUTS
+	Output objects from the following cmdlets:
+	Get-VMHost, Get-VM, Get-DistributedSwitch, Get-Datastore and Get-View -ViewType HostSystem.
+.OUTPUTS
+	[System.Management.Automation.PSCustomObject] PSObject collection.
+.NOTES
+	Author       ::	Roman Gelman.
+	Version 1.0  ::	23-May-2016  :: Release.
+.LINK
+	https://goo.gl/Yg7mYp
+#>
+
+[CmdletBinding(DefaultParameterSetName='VIO')]
+
+Param (
+
+	[Parameter(Mandatory,Position=1,ValueFromPipeline=$true,ParameterSetName='VIO')]
+	$VIObject
+	,
+	[Parameter(Mandatory,Position=1,ParameterSetName='VC')]
+	[switch]$VCenter
+	,
+	[Parameter(Mandatory,Position=1,ParameterSetName='LIC')]
+	[switch]$LicenseKey
+)
+
+Begin {
+
+	$ErrorActionPreference = 'SilentlyContinue'
+	
+	Function Get-VersionVMHostImpl {
+	Param ([Parameter(Mandatory,Position=1)]$InputObject)
+	$ErrorActionPreference = 'Stop'
+	Try
+		{
+			If ('Connected','Maintenance' -contains $InputObject.ConnectionState -and $InputObject.PowerState -eq 'PoweredOn') {
+				$ProductInfo = $InputObject.ExtensionData.Config.Product
+				$ProductVersion = [version]($ProductInfo.Version + '.' + $ProductInfo.Build)
+				
+				$Properties = [ordered]@{
+					ProductName = $InputObject.Name
+					ProductType = $ProductInfo.Name
+					FullVersion = $ProductInfo.FullName
+					Version     = $ProductVersion
+				}
+			}
+			Else {
+				$Properties = [ordered]@{
+					ProductName = $InputObject.Name
+					ProductType = 'VMware ESXi'
+					FullVersion = 'Unknown'
+					Version     = [version]'0.0.0.0'
+				}
+			}
+		}
+	Catch
+		{
+			$Properties = [ordered]@{
+				ProductName = $InputObject.Name
+				ProductType = 'VMware ESXi'
+				FullVersion = 'Unknown'
+				Version     = [version]'0.0.0.0'
+			}
+		}
+	Finally
+		{
+			$Object = New-Object PSObject -Property $Properties
+			$Object
+		}
+		
+	} #EndFunction Get-VersionVMHostImpl
+	
+	Function Get-VersionVMHostView {
+	Param ([Parameter(Mandatory,Position=1)]$InputObject)
+	$ErrorActionPreference = 'Stop'
+	Try
+		{
+			$ProductRuntime = $InputObject.Runtime
+			If ('connected','maintenance' -contains $ProductRuntime.ConnectionState -and $ProductRuntime.PowerState -eq 'poweredOn') {
+				$ProductInfo = $InputObject.Config.Product
+				$ProductVersion = [version]($ProductInfo.Version + '.' + $ProductInfo.Build)
+				
+				$Properties = [ordered]@{
+					ProductName = $InputObject.Name
+					ProductType = $ProductInfo.Name
+					FullVersion = $ProductInfo.FullName
+					Version     = $ProductVersion
+				}
+			}
+			Else {
+				$Properties = [ordered]@{
+					ProductName = $InputObject.Name
+					ProductType = 'VMware ESXi'
+					FullVersion = 'Unknown'
+					Version     = [version]'0.0.0.0'
+				}
+			}
+		}
+	Catch
+		{
+			$Properties = [ordered]@{
+				ProductName = $InputObject.Name
+				ProductType = 'VMware ESXi'
+				FullVersion = 'Unknown'
+				Version     = [version]'0.0.0.0'
+			}
+		}
+	Finally
+		{
+			$Object = New-Object PSObject -Property $Properties
+			$Object
+		}
+		
+	} #EndFunction Get-VersionVMHostView
+	
+	Function Get-VersionVM {
+	Param ([Parameter(Mandatory,Position=1)]$InputObject)
+	$ErrorActionPreference = 'Stop'
+	Try
+		{
+			$ProductInfo = $InputObject.Guest
+			
+			If ($InputObject.ExtensionData.Guest.ToolsStatus -ne 'toolsNotInstalled' -and $ProductInfo) {	
+				$ProductVersion = [version]$ProductInfo.ToolsVersion
+				
+				$Properties = [ordered]@{
+					ProductName = $InputObject.Name
+					ProductType = $InputObject.ExtensionData.Config.GuestFullName  #$ProductInfo.OSFullName
+					FullVersion = "VMware VM " + $InputObject.Version
+					Version     = $ProductVersion
+				}
+			}
+			Else {
+			
+				$Properties = [ordered]@{
+					ProductName = $InputObject.Name
+					ProductType = $InputObject.ExtensionData.Config.GuestFullName
+					FullVersion = "VMware VM " + $InputObject.Version
+					Version     = [version]'0.0.0'
+				}
+			}
+		}
+	Catch
+		{
+			$Properties = [ordered]@{
+				ProductName = $InputObject.Name
+				ProductType = 'Unknown'
+				FullVersion = 'VMware VM'
+				Version     = [version]'0.0.0'
+			}
+		}
+	Finally
+		{
+			$Object = New-Object PSObject -Property $Properties
+			$Object
+		}
+		
+	} #EndFunction Get-VersionVM
+	
+	Function Get-VersionPowerCLi {
+	$ErrorActionPreference = 'Stop'
+		Try
+			{
+				$PCLi = Get-PowerCLIVersion
+				$PCLiVer = [string]$PCLi.Major + '.' + [string]$PCLi.Minor + '.' + [string]$PCLi.Revision + '.' + [string]$PCLi.Build
+				
+				$Properties = [ordered]@{
+					ProductName = $env:COMPUTERNAME
+					ProductType = 'VMware vSphere PowerCLi'
+					FullVersion = $PCLi.UserFriendlyVersion
+					Version     = [version]$PCLiVer
+				}
+				$Object = New-Object PSObject -Property $Properties
+				$Object
+			}
+		Catch {}	
+	} #EndFunction Get-VersionPowerCLi
+	
+	Function Get-VersionVCenter {
+	Param ([Parameter(Mandatory,Position=1)]$InputObject)
+	$ErrorActionPreference = 'Stop'
+	Try
+		{
+			If ($obj.IsConnected) {
+				$ProductInfo = $InputObject.ExtensionData.Content.About
+				$ProductVersion = [version]($ProductInfo.Version + '.' + $ProductInfo.Build)
+				Switch -regex ($ProductInfo.OsType) {
+					'^win'   {$ProductFullName = $ProductInfo.Name + ' Windows'   ;Break}
+					'^linux' {$ProductFullName = $ProductInfo.Name + ' Appliance' ;Break}
+					Default  {$ProductFullName = $ProductInfo.Name                ;Break}
+				}
+				
+				$Properties = [ordered]@{
+					ProductName = $InputObject.Name
+					ProductType = $ProductFullName
+					FullVersion = $ProductInfo.FullName
+					Version     = $ProductVersion
+				}
+			}
+			Else {
+				$Properties = [ordered]@{
+					ProductName = $InputObject.Name
+					ProductType = 'VMware vCenter Server'
+					FullVersion = 'Unknown'
+					Version     = [version]'0.0.0.0'
+				}
+			}
+		}
+	Catch
+		{
+			$Properties = [ordered]@{
+				ProductName = $InputObject.Name
+				ProductType = 'VMware vCenter Server'
+				FullVersion = 'Unknown'
+				Version     = [version]'0.0.0.0'
+			}
+		}
+	Finally
+		{
+			$Object = New-Object PSObject -Property $Properties
+			$Object
+		}
+		
+	} #EndFunction Get-VersionVCenter
+	
+	Function Get-VersionVDSwitch {
+	Param ([Parameter(Mandatory,Position=1)]$InputObject)
+	$ErrorActionPreference = 'Stop'
+	$ProductTypeName = 'VMware DVSwitch'
+	Try
+		{
+			$ProductInfo = $InputObject.Summary.ProductInfo
+			$ProductFullVersion = 'VMware Distributed Virtual Switch ' + $ProductInfo.Version + ' build-' + $ProductInfo.Build
+			$ProductVersion = [version]($ProductInfo.Version + '.' + $ProductInfo.Build)
+			
+			$Properties = [ordered]@{
+				ProductName = $InputObject.Name
+				ProductType = $ProductTypeName
+				FullVersion = $ProductFullVersion
+				Version     = $ProductVersion
+			}
+		}
+	Catch
+		{
+			$Properties = [ordered]@{
+				ProductName = $InputObject.Name
+				ProductType = $ProductTypeName
+				FullVersion = 'Unknown'
+				Version     = [version]'0.0.0.0'
+			}
+		}
+	Finally
+		{
+			$Object = New-Object PSObject -Property $Properties
+			$Object
+		}
+		
+	} #EndFunction Get-VersionVDSwitch
+	
+	Function Get-VersionDatastore {
+	Param ([Parameter(Mandatory,Position=1)]$InputObject)
+	$ErrorActionPreference = 'Stop'
+	$ProductTypeName = 'VMware VMFS Datastore'
+	Try
+		{
+			$ProductVersionNumber = $InputObject.FileSystemVersion
+			$ProductFullVersion = 'VMware Datastore VMFS v' + $ProductVersionNumber
+			$ProductVersion = [version]$ProductVersionNumber
+			
+			$Properties = [ordered]@{
+				ProductName = $InputObject.Name
+				ProductType = $ProductTypeName
+				FullVersion = $ProductFullVersion
+				Version     = $ProductVersion
+			}
+		}
+	Catch
+		{
+			$Properties = [ordered]@{
+				ProductName = $InputObject.Name
+				ProductType = $ProductTypeName
+				FullVersion = 'Unknown'
+				Version     = [version]'0.0'
+			}
+		}
+	Finally
+		{
+			$Object = New-Object PSObject -Property $Properties
+			$Object
+		}
+		
+	} #EndFunction Get-VersionDatastore
+	
+	Function Get-VersionLicenseKey {
+	Param ([Parameter(Mandatory,Position=1)]$InputObject)
+	$ErrorActionPreference = 'Stop'
+	$ProductTypeName = 'License Key'
+	Try
+		{
+			$InputObjectProp = $InputObject |select -ExpandProperty Properties
+			Foreach ($prop in $InputObjectProp) {
+				If ($prop.Key -eq 'ProductName')        {$ProductType    = $prop.Value + ' ' + $ProductTypeName}
+				ElseIf ($prop.Key -eq 'ProductVersion') {$ProductVersion = [version]$prop.Value}
+			}
+			
+			Switch -regex ($InputObject.CostUnit) {
+				'^cpu'     {$LicCostUnit = 'CPU'; Break}
+				'^vm'      {$LicCostUnit = 'VM'; Break}
+				'server'   {$LicCostUnit = 'SRV'; Break}
+				Default    {$LicCostUnit = $InputObject.CostUnit}
+			
+			}
+			
+			$ProductFullVersion = $InputObject.Name + ' [' + $InputObject.Used + '/' + $InputObject.Total + $LicCostUnit + ']'
+			
+			$Properties = [ordered]@{
+				ProductName = $InputObject.LicenseKey
+				ProductType = $ProductType
+				FullVersion = $ProductFullVersion
+				Version     = $ProductVersion
+			}
+		}
+	Catch
+		{
+			$Properties = [ordered]@{
+				ProductName = $InputObject.LicenseKey
+				ProductType = $ProductTypeName
+				FullVersion = 'Unknown'
+				Version     = [version]'0.0'
+			}
+		}
+	Finally
+		{
+			$Object = New-Object PSObject -Property $Properties
+			If ($InputObject.EditionKey -ne 'eval') {$Object}
+		}
+		
+	} #EndFunction Get-VersionLicenseKey
+	
+}
+
+Process {
+
+	If ($PSCmdlet.ParameterSetName -eq 'VIO') {
+		Foreach ($obj in $VIObject) {
+			If     ($obj -is 'VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost')                  {Get-VersionVMHostImpl -InputObject $obj}
+			ElseIf ($obj -is 'VMware.Vim.HostSystem')                                                  {Get-VersionVMHostView -InputObject $obj}
+			ElseIf ($obj -is 'VMware.VimAutomation.ViCore.Types.V1.Inventory.VirtualMachine')          {Get-VersionVM -InputObject $obj}
+			ElseIf ($obj -is 'VMware.Vim.VmwareDistributedVirtualSwitch')                              {Get-VersionVDSwitch -InputObject $obj}
+			ElseIf ($obj -is 'VMware.VimAutomation.ViCore.Types.V1.DatastoreManagement.VmfsDatastore') {Get-VersionDatastore -InputObject $obj}
+			Else   {Write-Warning "Not supported object type"}
+		}
+	}
+	ElseIf ($PSCmdlet.ParameterSetName -eq 'VC') {
+		If ($global:DefaultVIServers.Length) {Foreach ($obj in $global:DefaultVIServers) {Get-VersionVCenter -InputObject $obj}}
+		Else {Write-Warning "Please use 'Connect-VIServer' cmdlet to connect to VCenter servers or ESXi hosts."}
+		Get-VersionPowerCLi
+	}
+	ElseIf ($PSCmdlet.ParameterSetName -eq 'LIC') {
+		If ($global:DefaultVIServers.Length) {Foreach ($obj in ((Get-View (Get-View ServiceInstance).Content.LicenseManager).Licenses)) {Get-VersionLicenseKey -InputObject $obj}}
+		Else {Write-Warning "Please use 'Connect-VIServer' cmdlet to connect to VCenter servers or ESXi hosts."}
+	}
+}
+
+End {}
+
+} #EndFunction Get-Version
+New-Alias -Name Get-ViMVersion -Value Get-Version -Force:$true
 
 Export-ModuleMember -Alias '*' -Function '*'
